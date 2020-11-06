76793f6c3b695901bda23168c812f3ea6f9c1131
premierboy <joshua.mwangangi@students.jkuat.ac.ke>
2020-11-06 15:48:24 +0300

    restored

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import './App.css';
-import Header from './Header';
-import SubBodySlider from "./SubBodySlider"
-import HomeProduct from './HomeProduct';
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <SubBodySlider />
-      <HomeProduct />
-    </div>
-  );
+import React ,{Component} from "react";
+import "./App.css";
+import Header from "./Header";
+import SubBodySlider from "./SubBodySlider";
+import HomeProduct from "./HomeProduct";
+import data from "./data.json";
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			products: data.products,
+			size: "",
+			sort:""
+		};
+	}
+	render() {
+		return (
+			<div className="app">
+				<Header />
+				<SubBodySlider />
+				<HomeProduct products={this.state.products}/>
+			</div>
+		);
+	}
 }
-
-export default App;
